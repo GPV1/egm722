8bdb6266a362ef742121cce9877a0d29c7b5482f
GPV1 <128035507+GPV1@users.noreply.github.com>
2023-03-27 11:43:05 +0200

    Added guess count to guessing game
    - changed while to for loop
    - Guesses taken in range of 10
    - added text to count guesses

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -3,16 +3,18 @@ import random
 # pick a random number for the user to guess
 rand = random.randint(1, 20)
 
-print('Guess a number between 1 and 20.')
-guess = int(input())  # number needs to be an integer
+for guessesTaken in range(1, 10):
+    print('Guess a number between 1 and 20.')
+    guess = int(input())  # number needs to be an integer
 
-while guess != rand:  # if the guess is not equal to the random number, you have to guess again
     if guess > rand:  # if the guess is too high, tell the user.
         print('Too high. Guess again.')
-    else:  # if the guess is too low, tell the user.
+    elif guess < rand:  # if the guess is too low, tell the user.
         print('Too low. Guess again.')
+    else:
+        break # this condition is the right guess
 
-    print('Enter a new guess: ')
-    guess = int(input())
-
-print('You got it! The number was {}'.format(rand))
+if guess == rand:
+    print('You got it! The number was {}'.format(rand) + ' You have guessed it in ' + str(guessesTaken) + ' guesses!')
+else:
+    print('Nope, the number was' + str(rand))
